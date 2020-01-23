9f91e0c7dcf20fe0fd8188984ffcfa4b6842a603
mani16 <mani16@ru.is>
2020-01-23 16:38:59 +0000

    sma redux komið

--- client/src/Components/PizzaPrices.jsx
+++ client/src/Components/PizzaPrices.jsx
@@ -1,18 +1,14 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
-export class PizzaPrices extends Component {
-    getToppings = () => {
-        var kukur = this.props.location.state;
-        alert(kukur);
-    }
-    render() {
-        return (
-            <div>
-                <Link  onClick={this.getToppings}>Finna Pizzu</Link>
-                <h1>{this.getToppings}</h1>
-            </div>
-        )
-    }
-}
+import {useSelector, useDispatch} from 'react-redux'
 
+function PizzaPrices(){
+    const toppings = useSelector(state => state.toppings);
+    return(
+        <div>
+            <h1>{toppings}</h1>
+        </div>  
+    )
+}
 export default PizzaPrices
+
