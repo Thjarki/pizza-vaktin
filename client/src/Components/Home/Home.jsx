ed175410fc9cf1c6bc5dfa68ef30c9599b14feed
mani16 <mani16@ru.is>
2020-01-28 15:57:03 +0000

    fullt af shitti, homepage og pizzapricezzzz, plain text password

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,34 +1,57 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import styles from './Home.module.scss'
-import {useSelector, useDispatch} from 'react-redux'
+import {useSelector, useDispatch, connect} from 'react-redux'
 import {addToppings, deleteToppings} from '../Actions'
 
-function Home(){
-    const dispatch = useDispatch();
-    var checkToppings = () => {
-        for(var i = 1; i < 4; i++){
+
+class Home extends Component {
+    state = {
+        toppings:  {
+            data: []
+        }  
+     }
+    checkToppings = () => {
+        for(var i = 1; i < this.state.toppings.data.length; i++){
             if (document.getElementById(i).checked){
-               dispatch(addToppings(document.getElementById(i).value));
+               this.props.dispatch(addToppings(document.getElementById(i).value));
             }else{
-                dispatch(deleteToppings(document.getElementById(i).value))
+                this.props.dispatch(deleteToppings(document.getElementById(i).value))
                 //delete not checked topping
             }
         }
         
     }
-    return(
-        <div className={styles.navContainer}>
-        <h1> Hvað má bjóða þér á pizzuna þína? </h1>
-        <div className={styles.navContainer}> 
-        <input type="checkbox" id="1" name="alegg1" value="Ostur" onChange={checkToppings} />Ostur
-        <input type="checkbox" id="2" name="alegg2" value="Sosa" onChange={checkToppings} />Sósa
-        <input type="checkbox" id="3" name="alegg3" value="Skinka" onChange={checkToppings} />Skinka
+    componentDidMount = () => {
+        fetch('http://206.189.19.13:5000/api/Toppings')
+        .then(res => res.json())
+        .then((data) => {
+            this.setState({ toppings : data })
+        })
+        .catch(console.log)
+    }
+
+
+    render() {
+        
+        const items = [];
+        console.log(this.state.toppings.data.length);
+        for(var i = 0; i < this.state.toppings.data.length; i++){   
+            var temp = this.state.toppings.data[i].name;
+            items.push(<div> <input type="checkbox" key={i} id={i} value={this.state.toppings.data[i].name} onChange={this.checkToppings}/> {temp} </div>);
+        }
+        return(
+            <div className={styles.navContainer}>
+            <h1> Hvað má bjóða þér á pizzuna þína? </h1>
+            <div className={styles.navContainer}> 
+                {items}
+            </div>
+            <div className={styles.navContainer}>
+                <Link className={styles.navbuttons} to="/pizzaverd" >Finna Pizzu</Link>
+            </div>
         </div>
-        <div className={styles.navContainer}>
-            <Link className={styles.navbuttons} to="/pizzaverd" >Finna Pizzu</Link>
-        </div>
-    </div>
-    )
+        )
+    }
+    
 }
-export default Home;
+export default connect()(Home);
