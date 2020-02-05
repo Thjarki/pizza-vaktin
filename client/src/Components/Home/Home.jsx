856aa5ff192a178c9a5c22d9d213c37c3774d09f
eybbus <eythorfreyr94@gmail.com>
2020-02-05 12:55:54 +0000

    Replaced old checkboxes with new one

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -12,22 +12,31 @@ import {
   emptyToppingArray
 } from "../Actions";
 
+import Checkbox from "../Checkbox/Checkbox";
+
 class Home extends Component {
   state = {
     toppings: {
       data: []
     }
   };
-  checkToppings = () => {
-    for (var i = 0; i < this.state.toppings.data.length; i++) {
-      if (document.getElementById(i).checked) {
-        this.props.dispatch(addToppings(document.getElementById(i).value));
-      } else {
-        this.props.dispatch(deleteToppings(document.getElementById(i).value));
-        //delete not checked topping
-      }
+  checkToppings = event => {
+    // TODO: handle "empty" Should be fixed by backend!
+    if (event.target.checked) {
+      this.props.dispatch(addToppings(event.target.name));
+    } else {
+      this.props.dispatch(deleteToppings(event.target.name));
     }
   };
+
+  radioChange = event => {
+    if (event.target.id === "N") {
+      this.checkNorth();
+    } else {
+      this.checkSouth();
+    }
+  };
+
   checkNorth = () => {
     if (document.getElementById("N").checked) {
       document.getElementById("S").checked = false;
@@ -53,25 +62,37 @@ class Home extends Component {
   checkLessToppings = () => {
     if (document.getElementById("akkurat").checked) {
       document.getElementById("meira").checked = false;
-      this.props.dispatch(deleteSpecificTopping(document.getElementById("meira").value));
+      this.props.dispatch(
+        deleteSpecificTopping(document.getElementById("meira").value)
+      );
       //setja inn í redux
-      this.props.dispatch(addSpecificTopping(document.getElementById("akkurat").value));
+      this.props.dispatch(
+        addSpecificTopping(document.getElementById("akkurat").value)
+      );
     } else {
       //eyða frá redux
-      this.props.dispatch(deleteSpecificTopping(document.getElementById("akkurat").value));
+      this.props.dispatch(
+        deleteSpecificTopping(document.getElementById("akkurat").value)
+      );
     }
-  }
+  };
   checkMoreToppings = () => {
     if (document.getElementById("meira").checked) {
       document.getElementById("akkurat").checked = false;
-      this.props.dispatch(deleteSpecificTopping(document.getElementById("akkurat").value));
+      this.props.dispatch(
+        deleteSpecificTopping(document.getElementById("akkurat").value)
+      );
       //setja inn í redux
-      this.props.dispatch(addSpecificTopping(document.getElementById("meira").value));
+      this.props.dispatch(
+        addSpecificTopping(document.getElementById("meira").value)
+      );
     } else {
       //eyða frá redux
-      this.props.dispatch(deleteSpecificTopping(document.getElementById("meira").value));
+      this.props.dispatch(
+        deleteSpecificTopping(document.getElementById("meira").value)
+      );
     }
-  }
+  };
   componentDidMount = () => {
     this.checkMoreToppings();
     this.props.dispatch(emptyToppingArray(" "));
@@ -89,59 +110,46 @@ class Home extends Component {
     for (var i = 0; i < this.state.toppings.data.length; i++) {
       var temp = this.state.toppings.data[i].name;
       items.push(
-        <div>
-          {" "}
-          <input
-            type="checkbox"
-            key={i}
-            id={i}
-            value={this.state.toppings.data[i].name}
-            onChange={this.checkToppings}
-          />{" "}
-          {temp}{" "}
-        </div>
+        <Checkbox
+          value={this.state.toppings.data[i].name}
+          onChange={this.checkToppings}
+        />
       );
     }
     return (
       <div className={styles.navContainer}>
         <div>
           <h1>Hvaðan viltu panta pizzu?</h1>
-          <div>
-            <input
-              type="checkbox"
-              key="N"
-              id="N"
-              value="norðurland"
-              onChange={this.checkNorth}
-            />{" "}
-            Akureyri
-            <input
-              type="checkbox"
-              key="S"
-              id="S"
-              value="höfuðborgarsvæðið"
-              onChange={this.checkSouth}
-            />{" "}
+          <div onChange={e => this.radioChange(e)}>
+            {/* Should be radio button */}
+            <input type="radio" key="N" id="N" value="norðurland" /> Akureyri
+            <input type="radio" key="S" id="S" value="höfuðborgarsvæðið" />{" "}
             Höfuðborgarsvæðið
-          </div>      
+          </div>
         </div>
         <div>
-          <h1>Viltu bara fá það sem þú velur á pizzuna eða er í lagi að það séu fleiri álegg?</h1>
-          <input 
+          <h1>
+            Viltu bara fá það sem þú velur á pizzuna eða er í lagi að það séu
+            fleiri álegg?
+          </h1>
+          {/* Should be radio button */}
+          <input
             type="checkbox"
             key="akkurat"
             id="akkurat"
             value="akkurat"
             onChange={this.checkLessToppings}
-          /> Bara fá það sem ég vel
-          <input 
+          />{" "}
+          Bara fá það sem ég vel
+          <input
             type="checkbox"
             key="meira"
             id="meira"
             value="meira"
             checked="true"
             onChange={this.checkMoreToppings}
-          /> Ég vill að það séu fleiri álegg
+          />{" "}
+          Ég vill að það séu fleiri álegg
         </div>
         <h1> Hvað má bjóða þér á pizzuna þína? </h1>
         <div className={styles.navContainer}>{items}</div>
