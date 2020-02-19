c0aa8c40099320b103a6a067b2c73c65280b7e1b
mani16 <mani16@ru.is>
2020-02-19 15:14:40 +0000

    pizzaprices i home

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,7 +1,9 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Home.module.scss";
 import { connect } from "react-redux";
+import Card from "../PizzaCard/Card";
+import { useSelector } from "react-redux";
 import {
   addLocation,
   deleteLocation,
@@ -13,12 +15,22 @@ import Checkbox from "../Checkbox/Checkbox";
 import Category from "../Category/Category";
 
 class Home extends Component {
-  state = {
-    toppings: {
-      data: []
-    }
-  };
+  constructor(props) {
+    super(props)
+    this.myRef = React.createRef() 
 
+    this.state = {
+      toppings: {
+        data: []
+      },
+      pizzas: {
+        data: []
+      },
+      buttonClicked : false,
+      isEmpty : false
+    };
+  }
+  
   radioChange = event => {
     if (event.target.id === "N") {
       this.checkNorth();
@@ -54,6 +66,115 @@ class Home extends Component {
     this.props.dispatch(setSpecificTopping(event.target.checked));
   };
 
+  getPizzas = () => {
+    var link = "https://www.eybbus.com/api/Pizza?";
+    for (var o = 0; o < this.props.chosenToppings.length; o++) {
+      if (o === this.props.chosenToppings.length - 1) {
+        link += "filter-topping=" + this.props.chosenToppings[o];
+      } else {
+        link += "filter-topping=" + this.props.chosenToppings[o] + "&";
+      }
+    }
+    fetch(link)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ pizzas: data });
+        this.setState({buttonClicked : true});
+      })
+      .catch(console.log);
+
+  }
+
+  fillCard = () => {
+    console.log(this.state.pizzas.data)
+      const items = [];
+      var pushItems = {};
+      for (var i = 0; i < this.state.pizzas.data.length; i++) {
+        if (
+          this.state.pizzas.data[i].company.region === this.props.location[0] ||
+          this.props.location.length === 0 || 
+          this.state.pizzas.data[i].company.name === "Dominos"
+        ) {
+          var tempTopping = "";
+          for (var k = 0; k < this.state.pizzas.data[i].toppings.length; k++) {
+            if (k === this.state.pizzas.data[i].toppings.length - 1) {
+              tempTopping += this.state.pizzas.data[i].toppings[k].name;
+            } else {
+              tempTopping += this.state.pizzas.data[i].toppings[k].name + ", ";
+            }
+          }
+          var tempName = this.state.pizzas.data[i].name;
+          var tempCompany = this.state.pizzas.data[i].company.name;
+          var tempMidPrice = this.state.pizzas.data[i].prices.size_m;
+          var tempLargePrice = this.state.pizzas.data[i].prices.size_l;
+          var tempSmallPrice = this.state.pizzas.data[i].prices.size_s;
+          var tempXLPrice = this.state.pizzas.data[i].prices.size_xl;
+    
+          if (tempSmallPrice === null) {
+            tempSmallPrice = "Ekki í boði";
+          }
+          if (tempMidPrice === null) {
+            tempMidPrice = "Ekki í boði";
+          }
+          if (tempLargePrice === null) {
+            tempLargePrice = "Ekki í boði";
+          }
+          if (tempXLPrice === null) {
+            tempXLPrice = "Ekki í boði";
+          }
+          pushItems = {
+            name: tempName,
+            companyName: tempCompany,
+            topping: tempTopping,
+            smallPrice: tempSmallPrice,
+            midPrice: tempMidPrice,
+            bigPrice: tempLargePrice,
+            xlPrice: tempXLPrice
+          };
+          if (!this.props.specificTopping) {
+            items.push(pushItems);
+          } else if (
+            this.props.specificTopping &&
+            this.state.pizzas.data[i].toppings.length === this.state.toppings.length
+          ) {
+            items.push(pushItems);
+          }
+        }
+      }
+      console.log(items)
+      if(items.length == 0)
+      {
+        this.state.isEmpty = true;
+      }else{
+        this.state.isEmpty = false;
+      }
+      let duration = 0.2;
+      const cards = items.map(emp => {
+        duration += 0.3;
+        return (
+          <Card
+            key={emp.name}
+            name={emp.name}
+            companyName={emp.companyName}
+            topping={emp.topping}
+            smallPrice={emp.smallPrice}
+            midPrice={emp.midPrice}
+            bigPrice={emp.bigPrice}
+            xlPrice={emp.xlPrice}
+            animateDuration={duration}
+          />
+        );
+      });
+
+      if(this.state.isEmpty){
+        setTimeout(this.scrollToMyRef, 250);
+        return <h1>Engar pizzur fundust með þessum leitarskylirðum.</h1>
+      }else {
+        setTimeout(this.scrollToMyRef, 250);
+        return cards
+      }
+  }
+  scrollToMyRef = () => window.scrollTo(0, this.myRef.current.offsetTop)
   componentDidMount = () => {
     this.props.dispatch(emptyToppingArray(" "));
     fetch("https://www.eybbus.com/api/Toppings")
@@ -62,9 +183,26 @@ class Home extends Component {
         this.setState({ toppings: data });
       })
       .catch(console.log);
+    
+    /*var link = "https://www.eybbus.com/api/Pizza?";
+    for (var o = 0; o < this.props.chosenToppings.length; o++) {
+      if (o === this.props.chosenToppings.length - 1) {
+        link += "filter-topping=" + this.props.chosenToppings[o];
+      } else {
+        link += "filter-topping=" + this.props.chosenToppings[o] + "&";
+      }
+    }
+    console.log(link)
+    fetch(link)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ pizzas: data });
+      })
+      .catch(console.log);*/
   };
 
   render() {
+    console.log(this.state.buttonClicked)
     const { data } = this.state.toppings;
     const filters = ["sósur og krydd", "ostur", "kjöt", "grænt", "ýmislegt", null];
     const gat = [];
@@ -77,7 +215,7 @@ class Home extends Component {
       if(filters[i] === null){
         categoryname = "Óflokkað"
       }
-      console.log(i);
+     
       
       gat.push(
         <Category
@@ -105,14 +243,20 @@ class Home extends Component {
         </div>
         <h1> Hvað má bjóða þér á pizzuna þína? </h1>
         <div className={styles.categoryContainer}>{gat}</div>
-
+        
         <div className={styles.navContainer}>
-          <Link className={styles.navbuttons} to="/pizzaverd">
+          <Link className={styles.navbuttons}  onClick={this.getPizzas}>
             Finna Pizzu
           </Link>
+        <div ref={this.myRef}>
+            {this.state.buttonClicked ? this.fillCard() : ''} 
+        </div> 
         </div>
       </div>
     );
   }
 }
-export default connect()(Home);
+function mapStateToProps(state) {
+  return {chosenToppings : state.toppings, location : state.location, specificTopping : state.specificTopping}
+}
+export default connect(mapStateToProps)(Home);
